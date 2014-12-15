132da19ce214b452aea406b2d9650606762de301
DWingo01 <danielwingo.dw@gmail.com>
2014-12-15 15:07:04 -0500

    AJAX Working
    Got the pricing list on a button click

--- support.php
+++ support.php
@@ -31,71 +31,16 @@
       </nav><!--experimental nav-->
       <div class="row general-content">
         <p><img src="img/progress.png"/></p>
+        <a href="js/prices.html" class="more radius left button">Click for price listings</a>      
       </div>
  
-          <div class="row">
- 
-          <div class="large-12 show-for-small columns">
-          <h3>Header</h3><hr>
-        </div>
- 
-          <div class="large-3 small-6 columns">
-            <img src="img/level1.png">
-            <div class="panel">
-              <h3><span class="orange-highlight-small">5-20</span> accounts</h3>
-              <h3>20 projects a month</h3>
-              <h3>400gbs of Enterprise-level secure cloud storage</h3>
-              <h3>24/7/365 support nationwide</h3>
-              <h3><span class="orange-highlight">$18 per user</span></h3>
-            </div>
-          </div>
- 
-          <div class="large-3 small-6 columns">
-            <img src="img/level2.png">
-            <div class="panel">
-              <h3><span class="orange-highlight-small">20-80</span> accounts</h3>
-              <h3>50 projects a month</h3>
-              <h3>800gbs of Enterprise-level secure cloud storage</h3>
-              <h3>2 virtual machines for high-level services</h3>
-              <h3>24/7/365 support nationwide</h3>
-              <h3><span class="orange-highlight">$27 per user</span></h3>
-            </div>
-          </div>
- 
-          <div class="large-3 small-6 columns">
-            <img src="img/level3.png">
-            <div class="panel">
-              <h3><span class="orange-highlight-small">80-160</span> accounts</h3>
-              <h3>unlimited projects a month</h3>
-              <h3>100gbs email storage per user</h3>
-              <h3><span class="orange-highlight-small">10 iPhone 6</span> or <span class="orange-highlight-small">15 Google Nexus 6</span> smartphones for executives</h3>
-              <h3>800gbs of Enterprise-level secure cloud storage</h3>
-              <h3>2 virtual machines for high-level services</h3>
-              <h3>24/7/365 support nationwide</h3>
-              <h3><span class="orange-highlight">$40 per user</span></h3>
-            </div>
-          </div>
- 
-          <div class="large-3 small-6 columns">
-            <img src="img/level4.png">
-            <div class="panel">
-              <h3><span class="orange-highlight-small">160-500</span> accounts</h3>
-              <h3>unlimited projects a month</h3>
-              <h3>500gbs email storage per user</h3>
-              <h3><span class="orange-highlight-small">20 iPhone 6</span> or <span class="orange-highlight-small">25 Google Nexus 6</span> smartphones for executives. 60 Samsung Galaxy S5 smartphones for key employees</h3>
-              <h3>2-10+ tbs of Enterprise-level secure cloud storage</h3>
-              <h3>4-10 virtual machines for high-level services</h3>
-              <h3>24/7/365 support <span class="orange-highlight-small">worldwide</span></h3>
-              <h3><span class="orange-highlight">$80 per user</span></h3>
-            </div>
-          </div>
- 
-        </div>
+      <div class="dynamic"></div>
 
     </section>
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
+    <script src="js/main.js"></script>
     <script>$('body').fadeIn(1500);</script>
     <script>
       $(document).foundation();
